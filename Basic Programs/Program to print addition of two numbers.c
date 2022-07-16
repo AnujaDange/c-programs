ddc92c8ddbb53334fafeeda8d81dc644bf747e6c
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-07-16 11:28:21 +0530

    Add files via upload

---
+++ Basic Programs/Program to print addition of two numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int no1,no2,add;
+   printf("\nEnter two number=>");
+   scanf("%d%d",&no1,&no2);
+   add=no1+no2;
+   printf("\nAddition=%d",add);
+   return(0);
+}
