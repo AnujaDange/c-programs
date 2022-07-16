e264846b094cd0314ebf70ede1e0aa0b22184f75
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-07-16 11:20:33 +0530

    Add files via upload

---
+++ Basic Programs/student information table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\n\n=======Student Information Table========\n");
+    printf("\n  %d %10s  %2d %2d %2d %0.2f",1,"yashraj",88,75,79,87.40);
+    printf("\n  %d %10s  %2d %2d %2d %0.2f",2,"gayatri",85,79,82,85.46);
+    printf("\n  %d %10s  %2d %2d %2d %0.2f",3,"sanika",80,78,77,80.20);
+    printf("\n  %d %10s  %2d %2d %2d %0.2f",4,"vaishnavi",78,80,82,81.28);
+    printf("\n\n=========================================");
+    return(0);
+
+}
