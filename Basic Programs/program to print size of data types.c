9c3d5efe0c1d1f73e655702fcb785c1e53d04d2d
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-07-17 09:27:38 +0530

    Add files via upload

---
+++ Basic Programs/program to print size of data types.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a;
+    char my_name='A';
+    float C;
+    double D;
+    printf("\n Size of integer=%d",sizeof(a));
+    printf("\n Size of character=%d",sizeof(my_name));
+    printf("\n Size of float=%d",sizeof(C));
+    printf("\n Size of double=%d",sizeof(D));
+    return(0);
+}
