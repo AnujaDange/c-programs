9ce4eccb386e96a9ca0cc93f3fab4dfba592d377
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-07-17 09:34:20 +0530

    Add files via upload

---
+++ Loop Programs/print table of  2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int r,c=2,no;
+    printf("\n===========table of 2===========");
+    for(r=1;r<=10;r++)
+    {
+        printf("\n    %d",r*c);
+    }
+}
