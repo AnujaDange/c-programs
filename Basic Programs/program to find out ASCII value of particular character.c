48672a038226afc530dc341d2cf8ddbe55a9c781
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-07-18 18:45:00 +0530

    Add files via upload

---
+++ Basic Programs/program to find out ASCII value of particular character.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch=0;
+    printf("\n Enter character =>");
+    scanf("%c",&ch);
+    printf("\n character value %c=%d",ch,ch);
+    return(0);
+
+}
