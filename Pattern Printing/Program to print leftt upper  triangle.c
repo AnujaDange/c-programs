996fff73cdb34ee31c2578d55adfb0916e53bceb
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-07-19 13:21:20 +0530

    Add files via upload

---
+++ Pattern Printing/Program to print leftt upper  triangle.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j,x;
+    printf("\nEnter size for row=>");
+    scanf("%d",&x);
+    for(i=1;i<=x;i++)
+    {
+        for(j=1;j<=x;j++)
+        {
+            if(i+j<=6)
+            {
+                printf("*");
+            }
+            printf("  ");
+        }
+        printf("\n");
+    }
+    return(0);
+}
