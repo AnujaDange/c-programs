c6a51d88cd31cb69c9c763351055a230c39844e0
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-07-20 19:13:25 +0530

    Add files via upload

---
+++ Basic Programs/program for swapping of two numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=10,b=20,c;
+    printf("\n=======Number Before swapping======");
+    printf("\n     A=%d",a);
+    printf("\n     B=%d",b);
+    c=a;
+    a=b;
+    b=c;
+    printf("\n=======Number After swapping=======");
+    printf("\n     A=%d",a);
+    printf("\n     B=%d",b);
+
+    return(0);
+}
