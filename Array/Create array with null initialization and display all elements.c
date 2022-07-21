3490ce9af6297b8300f0eeb15b5362091e5fa38d
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-07-21 23:30:42 +0530

    Add files via upload

---
+++ Array/Create array with null initialization and display all elements.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int Values[5]={ };
+    printf("\n 1st array element=>%d",Values[0]);
+    printf("\n 2nd array element=>%d",Values[1]);
+    printf("\n 3rd array element=>%d",Values[2]);
+    printf("\n 4th array element=>%d",Values[3]);
+    printf("\n 5th array element=>%d",Values[4]);
+    return(0);
+}
