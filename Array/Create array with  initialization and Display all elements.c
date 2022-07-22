3c331defd8b49e8f0e646daf159b80b2e0e8dd3a
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-07-22 20:25:23 +0530

    Add files via upload

---
+++ Array/Create array with  initialization and Display all elements.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int ArrNo[5]={5,7,7,2,9};
+    printf("\n 1st Array Element=>%d",ArrNo[0]);
+    printf("\n 2nd Array Element=>%d",ArrNo[1]);
+    printf("\n 3rd Array Element=>%d",ArrNo[2]);
+    printf("\n 4th Array Element=>%d",ArrNo[3]);
+    printf("\n 5th Array Element=>%d",ArrNo[4]);
+
+    return(0);
+
+
+}
