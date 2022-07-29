38732eb34342d074531a3848b4ed0bfa433255b6
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-07-29 21:29:45 +0530

    Add files via upload

---
+++ Loop Programs/program to print alphabets.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char ch='0';
+    printf("\n*****ALPHABETS FROM A to Z*****");
+    for(ch='A' ;ch<='Z' ;ch++)
+    {
+        printf("\n     %c ",ch);
+    }
+    return(0);
+}
