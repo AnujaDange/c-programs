a58e21bd8085d0544aeb17ca900988204c737319
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-08-04 10:44:26 +0530

    Add files via upload

---
+++ Array/Create,accept,display array using loop with micro,find minimum element in that array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#define size 5
+ int main()
+ {
+    int bill[size]={0},min=0,i=0;
+    for(i=0; i<size; i++)
+    {
+        printf("\n Enter Bill No. %d= ",i+101);
+        scanf("%d",&bill[i]);
+    }
+    printf("\n **************Bills Generated Today***************");
+    for(i=0; i<size; i++)
+    {
+        printf("\n Amount of %d Bill = %d",i+1,bill[i]);
+        if(i==0 || min>bill[i])
+        {
+            min=bill[i];
+        }
+    }
+    printf("\n\n*****************************************************");
+    printf("\n Minimum Amount of Bill=> %d \n",min);
+    return(0);
+ }
+
