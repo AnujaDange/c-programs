38bef7718f00a63fb504ade38dac188fe6e95341
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-08-05 15:17:59 +0530

    Add files via upload

---
+++ Loop Programs/Program to enter day no. and display day of week (usinf switch).c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int day=0;
+    printf("\n Enter Day number which you want=>");
+    scanf("%d",&day);
+    switch(day)
+    {
+        case 1: printf("\n Monday ");
+        break;
+        case 2: printf("\n Tuesday");
+        break;
+        case 3: printf("\n Wednesday ");
+        break;
+        case 4: printf("\n Thursday ");
+        break;
+        case 5: printf("\n Friday ");
+        break;
+        case 6: printf("\n Saturday ");
+        break;
+        case 7: printf("\n Suunday");
+        break;
+        default:
+                printf("\n Ufff Invalid choice ");
+
+    }
+    return(0);
+}
