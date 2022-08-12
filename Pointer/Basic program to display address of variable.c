84da2bab006bc5af08c085895c6517ef51581020
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-08-12 20:45:58 +0530

    Add files via upload

---
+++ Pointer/Basic program to display address of variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    char a;
+    float p,q;
+    printf("\n a is stored at address %u",&a);
+    printf("\n x is stored at address %u",&x);
+    printf("\n p is stored at address %u",&p);
+    printf("\n q is stored at address %u",&q);
+    return(0);
+}
