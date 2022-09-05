1199f0eb77933a8da1deae9731cd305ed41ab684
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-09-05 22:44:43 +0530

    Add files via upload

---
+++ Array/create array without initialisation and display all elements.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+
+  int value[5]={ 5};
+  printf("\nEnter 1st no.=%d",value[0]);
+  printf("\nEnter 2nd no.=%d",value[1]);
+  printf("\nEnter 3rd no.=%d",value[2]);
+  printf("\nEnter 4th no.=%d",value[3]);
+  printf("\nEnter 5th no.=%d",value[4]);
+
+  return 0;
+}
+
